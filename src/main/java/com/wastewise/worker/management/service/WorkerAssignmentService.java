f3264ba4969cb4cc109da59b9ca6c2f158c08cbe
Advay Banugariya <advay.banugariya@cognizant.com>
2025-06-10 06:59:25 +0530

    Minor change in workerAssignmentRepository

--- src/main/java/com/wastewise/worker/management/service/WorkerAssignmentService.java
+++ src/main/java/com/wastewise/worker/management/service/WorkerAssignmentService.java
@@ -4,9 +4,14 @@ import com.wastewise.worker.management.dto.WorkerAssignmentDTO;
 
 public interface WorkerAssignmentService {
 
+    String assignWorkertoAssignment(String assignmentId, String workerId, WorkerAssignmentDTO dto);
+
     String updateSingleWorkerAssignment(String assignmentId, String oldWorkerId, String newWorkerId);
+
+    String updateBothWorkerAssignments(String assignmentId,
+                                       String oldWorkerId1, String oldWorkerId2,
+                                       String newWorkerId1, String newWorkerId2);
 
     String deleteWorkerAssignment(String assignmentId);
 
-    String assignWorkertoAssignment(String assignmentId, String workerId, WorkerAssignmentDTO dto);
 }
